da153a5cb1f3498c4acf6aceb87671825cfd3d0c
h.takei <take.mh.imagin-lv@docomo.ne.jp>
2020-08-01 23:24:42 +0900

    Updates

---
+++ precache-manifest.f31f3457d83fe8a0d631b1df67577397.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed05ad5bdd124e6ae830605f917c0cf7",
+    "url": "/turn-take.github.io/index.html"
+  },
+  {
+    "revision": "8b7f13b712ad098b476d",
+    "url": "/turn-take.github.io/static/css/main.60a63524.chunk.css"
+  },
+  {
+    "revision": "7a2c196b64312e1a74bb",
+    "url": "/turn-take.github.io/static/js/2.415d515d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/turn-take.github.io/static/js/2.415d515d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b7f13b712ad098b476d",
+    "url": "/turn-take.github.io/static/js/main.d4e7b551.chunk.js"
+  },
+  {
+    "revision": "e354342b4ef239797731",
+    "url": "/turn-take.github.io/static/js/runtime-main.32f34b4d.js"
+  }
+]);
