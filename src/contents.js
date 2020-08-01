41231f643714f949b91ac6d0f6ad41f759c34562
h.takei <take.mh.imagin-lv@docomo.ne.jp>
2020-08-01 23:55:48 +0900

    reactで作り直した

---
+++ src/contents.js
@@ -0,0 +1,63 @@
+const contentsDef = {
+    history : [
+        {
+            dt: "~2017/03",
+            dd: "都内の大学 経済学科卒業"
+        },
+        {
+            dt: "2017/04~",
+            dd: "都内SIer企業入社"
+        },
+        {
+            dt: "2017/06~",
+            dd: "ネット証券会社 FXシステム開発に常駐SEとして参画"
+        },
+        {
+            dt: "2020/10~",
+            dd: "転職予定"
+        }
+    ],
+    about : [
+        {
+            dt: "生年月日",
+            dd: "1995/01/07"
+        },
+        {
+            dt: "出身地",
+            dd: "千葉県 木更津市"
+        },
+        {
+            dt: "所在地",
+            dd: "埼玉県 川口市"
+        },
+        {
+            dt: "趣味",
+            dd: "アニメ/漫画/プログラミング/ジム/実況見ることetc..."
+        }
+    ],
+    skill : [
+        {
+            dt: "Java",
+            dd: "業務経験3年、Java Gold SE8保有"
+        },
+        {
+            dt: "JavaScript/HTML/CSS",
+            dd: "業務経験2年"
+        }
+    ],
+    links : [
+        {
+            dt: "github",
+            dd: "ここ",
+            isLink: true,
+            src: "https://github.com/turn-take"
+        },
+        {
+            dt: "qiita",
+            dd: "ここ",
+            isLink: true,
+            src: "https://qiita.com/turn-take" 
+        }
+    ]
+}
+export default contentsDef;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,176 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import contentsDef from './contents';
+
+class PortFolio extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menus : [
+        {
+          title : "HISTORY",
+          selected : false 
+        },
+        {
+          title : "ABOUT",
+          selected : false
+        },
+        {
+          title : "SKILL",
+          selected : false
+        },
+        {
+          title : "LINKS",
+          selected : false
+        }
+      ],
+      contents : contentsDef,
+      currentContents : null,
+      currentContentsName : null,
+      isMain : true
+    }
+  }
+  render() {
+    return (
+      <div className="portfolio">
+        <Navigation
+        menus={this.state.menus}
+        onClick={index => this.refleshSelected(index)}/>
+        <Contents 
+        isMain={this.state.isMain}
+        currentContentsName={this.state.currentContentsName}
+        currentContents={this.state.currentContents}
+        />
+      </div>
+    )
+  }
+  refleshSelected(index) {
+    const menus = this.state.menus.slice();
+    menus.forEach(menu => menu.selected = false);
+    menus[index].selected = true;
+    const contentsName = menus[index].title.toLocaleLowerCase();
+    this.setState({
+      menus : menus,
+      currentContents : contentsDef[contentsName],
+      currentContentsName : menus[index].title,
+      isMain : false
+    });
+  }
+}
+
+class Navigation extends React.Component {
+
+  render() {
+    return(
+      <div className="navigation">
+        <nav>
+          <NavigationUL
+            menus={this.props.menus}
+            onClick={index => this.props.onClick(index)}
+          />
+        </nav>
+      </div>);
+  }
+}
+
+class NavigationUL extends React.Component {
+  render() {
+    const menus = this.props.menus;
+    const list = [];
+    Array.from(menus).forEach(
+      (menu, index) => list.push(this.renderList(menu, index)));
+
+    return(
+      <ul className="nav-ul">
+        {list}
+      </ul>
+    );
+  }
+
+  renderList(menu, index) {
+    const title = menu.title;
+    const selected = menu.selected;
+    const className = selected ? "nav-li selected" : "nav-li";
+    return(
+      <li className={className} onClick={() => this.props.onClick(index)}>
+        {title}
+      </li>
+    );
+  }  
+}
+
+class Main extends React.Component {
+  render() {
+    return(
+      <div className="main">
+        <div className="name">
+          turn-take
+        </div>
+        <div className="sub">
+          PORTFOLIO
+        </div>
+      </div>
+    )
+  };
+}
+
+class Contents extends React.Component{
+  render() {
+    return (
+      <div className="contents">
+        {this.props.isMain ? <Main/> : <Content 
+        currentContentsName={this.props.currentContentsName}
+        currentContents={this.props.currentContents}/>}
+      </div>
+    )
+  }
+}
+
+class Content extends React.Component{
+  render() {
+    return (
+      <div>
+        <h2>
+          {this.props.currentContentsName}
+        </h2>
+        <dl className="defList">
+          {this.rendarList()}
+        </dl>
+      </div>
+    )
+  }
+
+  rendarList() {
+    const list = [];
+    Array.from(this.props.currentContents).forEach(obj => {
+      Object.keys(obj).forEach(key => {
+        switch (key) {
+          case "dt":
+            list.push(<dt>{obj[key]}</dt>);
+            break;
+          case "dd":
+            if(obj.isLink) {
+              list.push(
+                <dd>
+                  <a href={obj.src} target="blank">{obj[key]}</a>
+                </dd>
+              )
+            } else {
+              list.push(<dd>{obj[key]}</dd>);
+            }
+            break;
+          default:
+            break;
+        }
+      })
+    })
+    return list;
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <PortFolio/>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
